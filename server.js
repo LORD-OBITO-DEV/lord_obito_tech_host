a6167ae88126c84262b4833ec1a5cf50b04c8acf
オビト・デヴ卿🐉🫟 <nguessanjeanstephane87@gmail.com>
2025-11-06 22:18:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const paymentRoutes = require('./routes/paymentRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ Connecté à MongoDB"))
+.catch(err => console.error("❌ Erreur MongoDB :", err));
 
 // Middlewares
 app.use(cors());
