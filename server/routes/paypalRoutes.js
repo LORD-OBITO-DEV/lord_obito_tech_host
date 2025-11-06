8585ed8f7e52e6a9a0dff68a91a3d876bf7a6ac4
オビト・デヴ卿🐉🫟 <nguessanjeanstephane87@gmail.com>
2025-11-06 20:12:14 +0000

    Create paypalRoutes.js

---
+++ server/routes/paypalRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { createOrder, captureOrder } from '../controllers/paypalController.js';
+const router = express.Router();
+router.post('/create', createOrder);
+router.post('/capture', captureOrder);
+export default router;
